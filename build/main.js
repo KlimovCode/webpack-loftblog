e663082938ee698804d37d8b15a5485668ba0e42
Klimov Code <klimovcode@mail.ru>
2021-05-19 11:09:22 +0500

    install dependencies, start setting webpack config

---
+++ build/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var n=function(n,e){var t=document.createElement("ul");t.className="menu";var u="";return["Главная","Обо мне","Портфолио"].forEach((function(n){u+="\n        <ul>\n            <li>' + item + '</li>\n        </ul>"})),t.innerHTML=u,t}();document.body.appendChild(n)})();
---
+++ source/menu.js
@@ -0,0 +1,21 @@
+export default function (array, className) {
+    
+    var menu = document.createElement("ul");
+    
+    menu.className = className;
+    
+    var listItems = '';
+    
+    array.forEach(function(item) {
+    
+    listItems += `
+        <ul>
+            <li>' + item + '</li>
+        </ul>`;
+    
+    });
+
+    menu.innerHTML = listItems;
+
+    return menu;
+}
